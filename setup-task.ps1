134aa05ad644561a5a66508c20540e90b42f81ad
Programming-Sai <saiahprog6@gmail.com>
2025-04-12 15:08:00 +0000

    Adding setup file

---
+++ setup-task.ps1
@@ -0,0 +1,5 @@
+$taskName = "GitAutoPushTask"
+$scriptPath = "C:\Path\To\your\main-runner.bat"
+$trigger = New-ScheduledTaskTrigger -Daily -At 9am
+$action = New-ScheduledTaskAction -Execute $scriptPath
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Automatically pushes to GitHub daily." -User "$env:USERNAME" -RunLevel Highest
